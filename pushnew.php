d2d97cd188ddd2e5019969e808ee791ccf4b9ed8
k1-801 <k1-801@mail.ru>
2015-02-17 12:11:12 +0400

    PHP - накат 3

--- pushnew.php
+++ pushnew.php
@@ -1,1 +1,22 @@
- 
+<?php
+include("header.dat");
+$n = sizeof(load("news")) + 1;
+
+$fh = fopen("news/n".$n.".dat", "w");
+if(!fh)
+	exit;
+
+$subj = $_GET["subject"];
+$text = $_GET["text"];
+
+fwrite($fh, "<?php
+	\$nd[] = false;
+	\$nt[] = ".time().";
+	\$na[] = \"".$id."\";
+	\$ns[] = \"".$subj."\";
+	\$ntext[] = \"".$text."\";
+?>");
+fclose($fh);
+header("Location: /?page=".$page);
+exit;
+?>
